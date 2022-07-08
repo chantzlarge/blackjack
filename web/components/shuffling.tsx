78e235db666248262de3afd87c8671b0a6d510f2
Chantz Large <contact@chantzlarge.com>
2022-07-08 17:36:38 -0400

    Renamed 'Shuffling' component to 'WaitingOnShuffling' component ;

--- web/components/shuffling.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Shuffling() {
-  return (<p>shuffling!</p>)
-}
---
+++ web/components/waiting-on-shuffling.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function WaitingOnShuffling() {
+  return (<p>waiting on shuffling!</p>)
+}
